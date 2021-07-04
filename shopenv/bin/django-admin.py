27747b2c85f82008c3e2b586cfeb405e8f30074d
michael <mikle.karibov@inbox.ru>
2021-07-04 16:42:34 +0300

    create my awesome project

---
+++ shopenv/bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/Users/anymacstore/Downloads/django3-ecommerce-v2-master/shopenv/bin/python3
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
